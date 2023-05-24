fec387808b5c02303612818d757c4eae5554b02c
Firoz Palapra <firoz.palapra@nine.com.au>
2023-05-24 15:01:26 +1000

    Add redirect logic

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express();
 
 app.use(express.static('public'))
 
+app.get("/redirect", (req,res)=>{
+    let r = (Math.random() + 1).toString(36).substring(7);
+    if(req.query && req.query.articleId) {
+        r = `${req.query.articleId}[${r}]`
+    }
+    res.redirect(`https://testapplinks.onrender.com/test?articleId=${r}`)
+})
+
 app.listen(3000,()=>{
     console.log("Started")
 })
